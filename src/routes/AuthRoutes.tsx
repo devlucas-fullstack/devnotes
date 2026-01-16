cd0f92b93e44967d27cb1b4e4435f872ed32915b
Lucas Vinicius <lucasv2224@outlook.com>
2026-01-16 12:49:02 -0300

    feat: creates login route

--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { AuthLayout } from "../components/AuthLayout";
+import { SignIn } from "../pages/SignIn";
 import { SignUp } from "../pages/SignUp";
 
 export function AuthRoutes() {
   return (
     <Routes>
       <Route path="/" element={<AuthLayout />}>
-        <Route path="/" element={<SignUp />} />
+        <Route path="/" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
       </Route>
     </Routes>
   );
