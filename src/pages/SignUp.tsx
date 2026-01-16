a7d7cd2ad29841ca1fbae24c20a88e814dea7827
Lucas Vinicius <lucasv2224@outlook.com>
2026-01-16 17:04:29 -0300

    feat: revovers the input values

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,18 +1,53 @@
+import { useState } from "react";
+import { z, ZodError } from "zod";
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
 import { Link } from "react-router-dom";
 
 export function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+
+  function onSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    console.log(name, email, password);
+  }
+
   return (
-    <form className="w-full flex flex-col gap-4">
-      <Input required legend="name" placeholder="Seu nome" />
+    <form className="w-full flex flex-col gap-4" onSubmit={onSubmit}>
+      <Input
+        required
+        legend="name"
+        placeholder="Seu nome"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
       <Input
         required
         type="email"
         legend="e-mail"
-        placeholder="seu@e-mail.com"
+        placeholder="seu@email.com"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
       />
-      <Input required type="password" legend="senha" placeholder="123456" />
+      <Input
+        required
+        type="password"
+        legend="senha"
+        placeholder="123456"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Input
+        required
+        type="password"
+        legend="Confirme a senha"
+        placeholder="123456"
+        value={passwordConfirm}
+        onChange={(e) => setPasswordConfirm(e.target.value)}
+      />
       <Button type="submit">Cadastrar</Button>
 
       <Link
