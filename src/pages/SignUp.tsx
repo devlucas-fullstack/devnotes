e8c182fe469b44971d2d5999ccfd40a9d185016f
Lucas Vinicius <lucasv2224@outlook.com>
2026-01-16 12:52:21 -0300

    feat: creates registration interface

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,3 +1,26 @@
+import { Input } from "../components/Input";
+import { Button } from "../components/Button";
+import { Link } from "react-router-dom";
+
 export function SignUp() {
-  return <h1>SignUp</h1>;
+  return (
+    <form className="w-full flex flex-col gap-4">
+      <Input required legend="name" placeholder="Seu nome" />
+      <Input
+        required
+        type="email"
+        legend="e-mail"
+        placeholder="seu@e-mail.com"
+      />
+      <Input required type="password" legend="senha" placeholder="123456" />
+      <Button type="submit">Cadastrar</Button>
+
+      <Link
+        to="/"
+        className="text-center text-sm font-semibold text-gray-900 hover:text-black transition ease-linear mt-8"
+      >
+        Já tenho uma conta
+      </Link>
+    </form>
+  );
 }
