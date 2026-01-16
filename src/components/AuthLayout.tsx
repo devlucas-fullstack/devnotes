5f97b3299d2063fca30b88cfaef3007bea7d473c
Lucas Vinicius <lucasv2224@outlook.com>
2026-01-16 11:51:17 -0300

    feat: create authlayout

---
+++ src/components/AuthLayout.tsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import icon from "../../public/icon.svg";
+
+export function AuthLayout() {
+  return (
+    <div className="w-screen h-screen bg-gray-900 flex justify-center items-center">
+      <main className="w-full md:w-115.5 sm:mx-4 bg-gray-300 rounded-2xl p-8 flex flex-col justify-center items-center">
+        <div className="flex justify-center items-center gap-2.5">
+          <img src={icon} alt="Ícone de nota" className="w-5 h-5" />
+          <h1 className="text-lg font-bold">DevNotes</h1>
+        </div>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router-dom";
+import { AuthLayout } from "../components/AuthLayout";
 import { SignUp } from "../pages/SignUp";
 
 export function AuthRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<SignUp />} />
+      <Route path="/" element={<AuthLayout />}>
+        <Route path="/" element={<SignUp />} />
+      </Route>
     </Routes>
   );
 }
