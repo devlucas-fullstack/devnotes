50396b28f5bb450f5cbc743cb52f7c631dbbfd1f
Lucas Vinicius <lucasv2224@outlook.com>
2026-01-17 22:41:41 -0300

    feat: create route to dashboard

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
+import { useAuth } from "../hooks/auth";
 import { AuthRoutes } from "./AuthRoutes";
+import { DashboardRoutes } from "./DashboardRoutes";
 
 export function Routes() {
+  const { session } = useAuth();
+
   return (
     <BrowserRouter>
-      <AuthRoutes />
+      {session?.user ? <DashboardRoutes /> : <AuthRoutes />}
     </BrowserRouter>
   );
 }
