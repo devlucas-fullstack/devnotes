16e0d0edebbe57692b5d4a0051bf7f99ab3c66c0
Lucas Vinicius <lucasv2224@outlook.com>
2026-01-18 13:48:59 -0300

    feat: creates dashboard interface

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,3 +1,40 @@
+import { useNavigate } from "react-router-dom";
+
 export function Dashboard() {
-  return <h1>Dashboard</h1>;
+  const navigate = useNavigate();
+
+  function createNote() {
+    navigate("/create");
+  }
+
+  return (
+    <div>
+      <div className="w-full lg:max-w-270.5 flex flex-col mx-auto bg-gray-300 p-10 rounded-2xl my-8">
+        <h1 className="text-xl font-bold flex-1 border-b border-b-gray-400 pb-6">
+          Notas
+        </h1>
+        <div className="mt-6 w-full flex flex-row flex-wrap gap-5">
+          <p>Lorem ipsum dolor sit amet </p>
+        </div>
+      </div>
+      <button
+        type="button"
+        className="
+    fixed
+    bottom-4
+    right-4
+    bg-blue-600
+    text-white
+    px-4
+    py-2
+    rounded-full
+    shadow-lg
+    hover:bg-blue-700
+  "
+        onClick={createNote}
+      >
+        +
+      </button>
+    </div>
+  );
 }
