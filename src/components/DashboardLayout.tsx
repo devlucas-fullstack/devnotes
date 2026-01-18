e1766eb485713cfb1846a87c067869776a570977
Lucas Vinicius <lucasv2224@outlook.com>
2026-01-18 13:47:47 -0300

    feat: create dashboard layout

---
+++ src/components/DashboardLayout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import { Header } from "./Header";
+
+export function DashboardLayout() {
+  return (
+    <div className="w-screen h-screen bg-gray-900 flex flex-col items-center">
+      <main className="p-3 w-full">
+        <Header />
+        <Outlet />
+      </main>
+    </div>
+  );
+}
