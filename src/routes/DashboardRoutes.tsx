092fd28a13d0790193faa75f032f0368da8d4cf1
Lucas Vinicius <lucasv2224@outlook.com>
2026-01-18 12:42:34 -0300

    feat: create route dashboard layout

--- src/routes/DashboardRoutes.tsx
+++ src/routes/DashboardRoutes.tsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router-dom";
+import { DashboardLayout } from "../components/DashboardLayout";
 import { Dashboard } from "../pages/Dashboard";
 
 export function DashboardRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
+      <Route path="/" element={<DashboardLayout />}>
+        <Route path="/" element={<Dashboard />} />
+      </Route>
     </Routes>
   );
 }
