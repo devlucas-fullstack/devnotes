ddc2302056fe5975823c80abb8793a07a31cdd2e
Lucas Vinicius <lucasv2224@outlook.com>
2026-01-18 22:04:57 -0300

    feat: establishes route for note creation

--- src/routes/DashboardRoutes.tsx
+++ src/routes/DashboardRoutes.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { DashboardLayout } from "../components/DashboardLayout";
 import { Dashboard } from "../pages/Dashboard";
+import { CreateNote } from "../pages/CreateNote";
 
 export function DashboardRoutes() {
   return (
     <Routes>
       <Route path="/" element={<DashboardLayout />}>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/create" element={<CreateNote />} />
       </Route>
     </Routes>
   );
